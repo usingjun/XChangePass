4db4ddb1545569caab9720938d08a72bf6ed3201
Si-rauis <kkyd7189@gmail.com>
2025-02-20 09:33:40 +0900

    ✨ Feat : DELETE, UPDATE 에러 코드 추가

--- src/main/java/bumblebee/xchangepass/global/error/ErrorCode.java
+++ src/main/java/bumblebee/xchangepass/global/error/ErrorCode.java
@@ -9,9 +9,11 @@ public enum ErrorCode {
     /*User*/
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "U001", "존재 하지 않는 회원입니다."),
     USER_NOT_MODIFY(HttpStatus.BAD_REQUEST, "U002", "회원 수정에 실패했습니다."),
-    USER_DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "U003", "중복된 이메일 입니다."),
-    USER_DUPLICATE_NICK_NAME(HttpStatus.BAD_REQUEST, "U004", "중복된 닉네임 입니다."),
-    USER_DUPLICATE_PHONE_NUMBER(HttpStatus.BAD_REQUEST, "U005", "중복된 전화번호 입니다."),
+    USER_NOT_REGISTER(HttpStatus.BAD_REQUEST, "U003", "회원가입에 실패했습니다."),
+    USER_NOT_DELETE(HttpStatus.BAD_REQUEST, "U004", "회원 삭제 요청에 실패했습니다."),
+    USER_DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "U005", "중복된 이메일 입니다."),
+    USER_DUPLICATE_NICK_NAME(HttpStatus.BAD_REQUEST, "U006", "중복된 닉네임 입니다."),
+    USER_DUPLICATE_PHONE_NUMBER(HttpStatus.BAD_REQUEST, "U007", "중복된 전화번호 입니다."),
 
     /*기타*/
     ENTITY_FIELD_ACCESS_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G001", "엔티티 필드 접근 오류");
