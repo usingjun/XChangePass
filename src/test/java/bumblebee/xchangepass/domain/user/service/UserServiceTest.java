0e8b4b45b3c37e5d62c0c0d5009053379cc3547e
Si-rauis <kkyd7189@gmail.com>
2025-03-04 09:42:57 +0900

    ✨ Feat : UserService 단 테스트 구성

---
+++ src/test/java/bumblebee/xchangepass/domain/user/service/UserServiceTest.java
@@ -0,0 +1,113 @@
+package bumblebee.xchangepass.domain.user.service;
+
+import bumblebee.xchangepass.config.RedisTestBase;
+import bumblebee.xchangepass.domain.user.dto.request.UserRegisterRequest;
+import bumblebee.xchangepass.domain.user.dto.request.UserUpdateRequest;
+import bumblebee.xchangepass.domain.user.entity.Sex;
+import bumblebee.xchangepass.domain.user.entity.User;
+import bumblebee.xchangepass.domain.user.repository.UserRepository;
+import bumblebee.xchangepass.global.error.ErrorCode;
+import bumblebee.xchangepass.global.exception.CommonException;
+import bumblebee.xchangepass.global.scheduler.UserCleanupScheduler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserServiceTest extends RedisTestBase {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserCleanupScheduler userCleanupScheduler;
+
+
+    @BeforeEach
+    void setUp() {
+        IntStream.rangeClosed(1, 5).forEach(i -> {
+                    UserRegisterRequest testUser = UserRegisterRequest.builder()
+                            .userEmail("Test" + i + "@gmail.com")
+                            .userPwd("Qwer1234!")
+                            .userName("테스터" + i)
+                            .userPhoneNumber("010-0000-000" + i)
+                            .userSex(i >= 4 ? "FEMALE" : "MALE")
+                            .build();
+
+                    userService.signupUser(testUser);
+                });
+    }
+
+
+    @Test
+    @DisplayName("회원 정보 수정 & 동적 쿼리 & 예외처리")
+    void checkForDuplicateNickname() {
+        Long userId1 = 1L;
+        Long userId2 = 2L;
+
+        // userId1 사용자의 닉네임을 "Test1"로 변경 (성공)
+        UserUpdateRequest firstUpdate = UserUpdateRequest.builder()
+                .userNickname("Test1")
+                .userAge(23)
+                .userSex(Sex.FEMALE)
+                .build();
+        assertDoesNotThrow(() -> userService.updateUser(userId1, firstUpdate));
+
+        User user1 = userRepository.findById(userId1).orElseThrow();
+        assertEquals("Test1", user1.getUserNickname().getValue(), "닉네임이 'Test1'로 업데이트되지 않았습니다.");
+
+
+        // userId2 사용자가 동일한 "Test1"로 변경 시 예외 발생
+        UserUpdateRequest duplicateUpdate = UserUpdateRequest.builder()
+                .userNickname("Test1")
+                .userAge(23)
+                .userSex(Sex.FEMALE)
+                .build();
+
+        CommonException exception = assertThrows(CommonException.class, () -> userService.updateUser(userId2, duplicateUpdate));
+        assertEquals(ErrorCode.USER_DUPLICATE_NICK_NAME, exception.getErrorCode(), "예외 코드가 잘못되었습니다.");
+
+        // 'User_'로 시작하는 닉네임 변경 시 예외 발생
+        UserUpdateRequest invalidPrefixUpdate = UserUpdateRequest.builder()
+                .userNickname("User_1000000001")
+                .userAge(23)
+                .userSex(Sex.FEMALE)
+                .build();
+
+        CommonException prefixException = assertThrows(CommonException.class, () -> userService.updateUser(userId2, invalidPrefixUpdate));
+        assertEquals(ErrorCode.INVALID_NICKNAME_PREFIX, prefixException.getErrorCode(), "예외 코드가 잘못되었습니다.");
+    }
+
+    @Test
+    @DisplayName("Soft Delete & Hard Delete 테스트")
+    void testSoftDeleteUser() {
+        Clock mockClock = Clock.fixed(LocalDateTime.now().plusDays(30).atZone(ZoneOffset.UTC).toInstant(), ZoneOffset.UTC);
+
+        userCleanupScheduler.setClock(mockClock);
+
+        Long userId = 1L;
+
+        User user = userRepository.findById(userId).orElseThrow();
+        user.softDelete();
+        userRepository.saveAndFlush(user);
+
+        userCleanupScheduler.deleteUser();
+
+        assertThrows(CommonException.class, () -> userRepository.findByUserId(userId).orElseThrow(ErrorCode.USER_NOT_FOUND::commonException));
+    }
+
+}
