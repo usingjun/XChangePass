be1e83853d58c25ea52726cd17e3d74782ef12bb
Si-rauis <kkyd7189@gmail.com>
2025-03-04 09:41:26 +0900

    ♻️ Refactor : User Service 주석 처리 및 트랜잭션 관리

--- src/main/java/bumblebee/xchangepass/domain/user/service/UserService.java
+++ src/main/java/bumblebee/xchangepass/domain/user/service/UserService.java
@@ -12,25 +12,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final NicknameGenerator nicknameGenerator;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     /*
     사용자 등록
-    실명의 경우 전화번호, 이메일 인증 시 받아오는 방식으로 구상 생각
+    닉네임 Redis INCR을 활요한 자동 생성
+    실명의 경우 추후 전화번호, 이메일에서 받아 오는 형식으로 변경 예정
      */
     public void signupUser(UserRegisterRequest request) {
         try{
-            userRepository.save(request.toEntity(bCryptPasswordEncoder));
-        }catch (DataIntegrityViolationException e) {
+            String uniqueNickname = nicknameGenerator.generateUniqueNickname();
+            userRepository.save(request.toEntity(bCryptPasswordEncoder, uniqueNickname));
+        } catch (DataIntegrityViolationException e) {
             DuplicateKeyExceptionHandler.handle(e);
+        } catch (IllegalArgumentException | CommonException e) {
+            throw e;
         } catch (Exception e) {
             throw ErrorCode.USER_NOT_REGISTER.commonException();
         }
@@ -48,10 +52,13 @@ public class UserService {
 
     /*
     사용자 정보 수정
-    전화번호 변경의 경우 휴대폰 인증 진행 후 따로 변경 예정
      */
     public void updateUser(Long userId, UserUpdateRequest request) {
-        //중복 검사
+
+        if (request.userNickname().startsWith("User_")) {
+            throw ErrorCode.INVALID_NICKNAME_PREFIX.commonException();
+        }
+
         userRepository.checkForDuplicateNickname(request.userNickname(), userId);
 
         try {
@@ -68,6 +75,7 @@ public class UserService {
     /*
     사용자 삭제 요청 (Soft Delete)
      */
+    @Transactional
     public void softDeleteUser(Long userId) {
         User existUser = userRepository.findById(userId)
                 .orElseThrow(ErrorCode.USER_NOT_FOUND::commonException);
