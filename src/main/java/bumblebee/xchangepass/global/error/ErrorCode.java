173a98609ec873863acb3af98382b9068b1cb0a4
CryingPerson <opnice12@naver.com>
2025-03-05 11:27:17 +0900

    ✨ Feat : ErrorCode 추가

--- src/main/java/bumblebee/xchangepass/global/error/ErrorCode.java
+++ src/main/java/bumblebee/xchangepass/global/error/ErrorCode.java
@@ -7,17 +7,27 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     /*Member*/
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "M003-1", "존재 하지 않는 회원입니다."),
-    USER_UPDATE_EXCEPTION(HttpStatus.BAD_REQUEST, "M001", "잘못된 회원 수정 입니다."),
-    USER_DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "L005", "중복된 이메일 입니다."),
-    USER_DUPLICATE_NICK_NAME(HttpStatus.BAD_REQUEST, "L006", "중복된 닉네임 입니다."),
-    USER_DUPLICATE_PHONENUMBER(HttpStatus.BAD_REQUEST, "L007", "중복된 전화번호 입니다."),
-    USER_NOT_MODIFY(HttpStatus.BAD_REQUEST,"L008","회원 수정 실패"),
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "U001", "존재 하지 않는 회원입니다."),
+    USER_UPDATE_EXCEPTION(HttpStatus.BAD_REQUEST, "U002", "잘못된 회원 수정 입니다."),
+    USER_DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "U003", "중복된 이메일 입니다."),
+    USER_DUPLICATE_NICK_NAME(HttpStatus.BAD_REQUEST, "U004", "중복된 닉네임 입니다."),
+    USER_DUPLICATE_PHONE_NUMBER(HttpStatus.BAD_REQUEST, "U005", "중복된 전화번호 입니다."),
+    USER_NOT_MODIFY(HttpStatus.BAD_REQUEST,"U006","회원 수정 실패"),
 
     //Balance
     BALANCE_NOT_FOUND(HttpStatus.BAD_REQUEST, "M005-1", "존재하지 않는 지갑입니다."),
     BALANCE_NOT_AVAILABLE(HttpStatus.BAD_REQUEST, "M005-2", "충전 금액이 부족합니다."),
 
+    //Exchange_rate
+    EXCHANGE_RATE_NOT_FOUND(HttpStatus.NOT_FOUND, "E001", "존재 하지 않는 환율입니다."),
+    EXCHANGE_RATE_FOR_COUNTRY(HttpStatus.BAD_REQUEST, "E003", "이 나라에 대한 환율 정보가 없습니다."),
+    EXCHANGE_SAVE_FAIL(HttpStatus.BAD_REQUEST, "E002", "환율 정보 저장 실패"),
+
+    //TRANSACTION
+    TRANSACTION_ALREADY_COMPLETED(HttpStatus.BAD_REQUEST, "T001", "이미 완료된 거래입니다."),
+    TRANSACTION_AMOUNT_NOTFOUND(HttpStatus.BAD_REQUEST,"T003", "환전 금액은 필수 입니다."),
+    TRANSACTION_HISTORY_NOT_FOUND(HttpStatus.NOT_FOUND, "T002", "존재 하지 않는 환전 내역입니다."),
+    TRANSACTION_USERID_NOT_FOUND(HttpStatus.NOT_FOUND, "T004", "회원 아이디 필수"),
     /*기타*/
     ENTITY_FIELD_ACCESS_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G001", "엔티티 필드 접근 오류");
 
