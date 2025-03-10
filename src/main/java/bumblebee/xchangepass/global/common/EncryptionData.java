bbbc6cf3a26ff0f2427a676ab1692fcc007b42fb
Si-rauis <kkyd7189@gmail.com>
2025-03-10 09:16:58 +0900

    ✨ Feat : 암호화 데이터 필드

---
+++ src/main/java/bumblebee/xchangepass/global/common/EncryptionData.java
@@ -0,0 +1,26 @@
+package bumblebee.xchangepass.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EncryptionData {
+
+    @Column(name = "encrypted_aes_key", nullable = false)
+    private String encryptedAesKey;
+
+    @Column(name = "iv", nullable = false)
+    private byte[] iv;
+
+    public EncryptionData(String encryptedAesKey, byte[] iv) {
+        this.encryptedAesKey = encryptedAesKey;
+        this.iv = iv;
+    }
+}
