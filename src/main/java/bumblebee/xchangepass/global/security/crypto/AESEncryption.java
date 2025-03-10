937b0924ce6cd4e9741dabcb61e6ef090a412c4b
Si-rauis <kkyd7189@gmail.com>
2025-03-10 12:00:25 +0900

    ✨ Feat : AES 암호화 클래스 예외처리

--- src/main/java/bumblebee/xchangepass/global/security/crypto/AESEncryption.java
+++ src/main/java/bumblebee/xchangepass/global/security/crypto/AESEncryption.java
@@ -1,5 +1,7 @@
 package bumblebee.xchangepass.global.security.crypto;
 
+import bumblebee.xchangepass.global.error.ErrorCode;
+
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -11,39 +13,55 @@ import java.util.Base64;
 public class AESEncryption {
 
     // AES-256 키 생성
-    public static SecretKey generateAESKey() throws Exception {
-        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-        keyGen.init(256);
-        return keyGen.generateKey();
+    public static SecretKey generateAESKey(){
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+            keyGen.init(256);
+            return keyGen.generateKey();
+        }catch (Exception e) {
+            throw ErrorCode.AES_KEY_GENERATION_FAILED.commonException();
+        }
     }
 
     // IV 생성
     public static byte[] generateIV() {
-        byte[] iv = new byte[16]; // AES 블록 크기는 16바이트
-        new SecureRandom().nextBytes(iv);
-        return iv;
+        try {
+            byte[] iv = new byte[16]; // AES 블록 크기는 16바이트
+            new SecureRandom().nextBytes(iv);
+            return iv;
+        }catch (Exception e) {
+            throw ErrorCode.IV_GENERATION_FAILED.commonException();
+        }
     }
 
     // AES CBC 모드를 사용한 데이터 암호화
-    public static String encryptData(String data, SecretKey secretAESKey, byte[] iv) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        SecretKeySpec secretKeySpec = new SecretKeySpec(secretAESKey.getEncoded(), "AES");
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+    public static String encryptData(String data, SecretKey secretAESKey, byte[] iv) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(secretAESKey.getEncoded(), "AES");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
 
-        byte[] encryptedData = cipher.doFinal(data.getBytes());
-        return Base64.getEncoder().encodeToString(encryptedData);
+            byte[] encryptedData = cipher.doFinal(data.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedData);
+        }catch (Exception e) {
+            throw ErrorCode.DATE_ENCRYPTION_FAILED.commonException();
+        }
     }
 
     // AES CBC 모드를 사용한 데이터 복호화
-    public static String decryptData(String encryptedData, SecretKey secretKey, byte[] iv) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec);
+    public static String decryptData(String encryptedData, SecretKey secretKey, byte[] iv) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec);
 
-        byte[] decodedEncryptedData = Base64.getDecoder().decode(encryptedData);
-        byte[] decryptedData = cipher.doFinal(decodedEncryptedData);
-        return new String(decryptedData).trim();
+            byte[] decodedEncryptedData = Base64.getDecoder().decode(encryptedData);
+            byte[] decryptedData = cipher.doFinal(decodedEncryptedData);
+            return new String(decryptedData).trim();
+        }catch (Exception e) {
+            throw ErrorCode.DATE_DECRYPTION_FAILED.commonException();
+        }
     }
 
 }
