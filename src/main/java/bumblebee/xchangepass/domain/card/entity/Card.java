d60ce9c523ec145e3f843db1786089bd1e5c43a3
Si-rauis <kkyd7189@gmail.com>
2025-03-10 09:17:11 +0900

    ✨ Feat : Card Entity 생성

--- src/main/java/bumblebee/xchangepass/domain/card/entity/Card.java
+++ src/main/java/bumblebee/xchangepass/domain/card/entity/Card.java
@@ -1,6 +1,8 @@
 package bumblebee.xchangepass.domain.card.entity;
 
+import bumblebee.xchangepass.global.common.EncryptionData;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
@@ -22,27 +24,52 @@ public class Card {
     @Column(name = "card_id", nullable = false)
     private Long cardId;
 
-    @Column(name = "card_number")
+    @Column(name = "card_number", nullable = false)
     private String cardNumber;
 
-    @Column(name = "cvc")
-    private Integer cvc;
+    @Column(name = "cvc", nullable = false)
+    private String cvc;
 
-    @Column(name = "owner_name")
+    @Column(name = "card_password", nullable = false)
+    private String cardPassword;
+
+    @Column(name = "owner_name", nullable = false)
     private String ownerName;
 
-    @Column(name = "card_type")
+    @Column(name = "card_type", nullable = false)
     @Enumerated(value = EnumType.STRING)
     private CardType cardType;
 
-    @Column(name = "card_status")
+    @Column(name = "card_status", nullable = false)
     @Enumerated(value = EnumType.STRING)
     private CardStatus cardStatus;
 
-    @Column(name = "expiration_date")
+    @Column(name = "expiration_date", nullable = false)
     private LocalDateTime expirationDate;
 
     @CreatedDate
     @Column(name = "card_create_date")
     private LocalDateTime cardCreateDate;
+
+    @Embedded
+    private EncryptionData encryptionData;
+
+    @Builder
+    public Card(String cardNumber,
+                String cvc,
+                String cardPassword,
+                String ownerName,
+                CardType cardType,
+                String encryptedAesKey,
+                byte[] iv){
+        this.cardNumber = cardNumber;
+        this.cvc = cvc;
+        this.cardPassword = cardPassword;
+        this.ownerName = ownerName;
+        this.cardType = cardType;
+        this.cardStatus = CardStatus.ACTIVE;
+        this.expirationDate = LocalDateTime.now().plusYears(5);
+        this.encryptionData = new EncryptionData(encryptedAesKey, iv);
+    }
+
 }
