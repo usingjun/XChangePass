06497abd821f0afd0b2cc31e81bc296c127cb3ec
usingjun <usingjun99@gmail.com>
2025-03-13 11:04:43 +0900

    :sparkles: Feat: RefreshToken Controller, Service

---
+++ src/main/java/bumblebee/xchangepass/global/security/v1/RefreshTokenService.java
@@ -0,0 +1,54 @@
+package bumblebee.xchangepass.global.security.v1;
+
+import bumblebee.xchangepass.global.error.ErrorCode;
+import bumblebee.xchangepass.global.security.JwtProvider;
+import bumblebee.xchangepass.global.security.v1.refresh.dto.RefreshTokenResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService  {
+
+    private final JwtProvider jwtProvider;
+
+    /**
+     * refresh token을 이용하여 access token, refresh token 재발급
+     *
+     * @param refreshToken refresh token
+     * @return RefreshTokenResponseDTO
+     */
+    public RefreshTokenResponse refreshToken(final String refreshToken) {
+        // refresh token 유효성 검증
+        checkRefreshToken(refreshToken);
+
+        // refresh token id 조회
+        var id = RefreshToken.getRefreshToken(refreshToken);
+
+        // 새로운 access token 생성
+        String newAccessToken = jwtProvider.generateAccessToken(id);
+
+        // 기존에 가지고 있는 사용자의 refresh token 제거
+        RefreshToken.removeUserRefreshToken(id);
+
+        // 새로운 refresh token 생성 후 저장
+        String newRefreshToken = jwtProvider.generateRefreshToken(id);
+        RefreshToken.putRefreshToken(newRefreshToken, id);
+
+        return RefreshTokenResponse.builder()
+                .accessToken(newAccessToken)
+                .refreshToken(newRefreshToken)
+                .build();
+    }
+
+    /**
+     * refresh token 검증
+     *
+     * @param refreshToken refresh token
+     */
+    private void checkRefreshToken(final String refreshToken) {
+        if(Boolean.FALSE.equals(jwtProvider.validateToken(refreshToken)))
+            throw ErrorCode.REFRESH_TOKEN_INVALID.commonException();
+    }
+
+}
---
+++ src/main/java/bumblebee/xchangepass/global/security/v1/refresh/RefreshTokenController.java
@@ -0,0 +1,23 @@
+package bumblebee.xchangepass.global.security.v1.refresh;
+
+import bumblebee.xchangepass.global.security.v1.RefreshTokenService;
+import bumblebee.xchangepass.global.security.v1.refresh.dto.RefreshTokenRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RefreshTokenController {
+
+    private final RefreshTokenService refreshTokenService;
+
+    @PostMapping("/token-refresh")
+    public void tokenRefresh(@RequestBody @Valid RefreshTokenRequest request) {
+        // token 재발급
+        refreshTokenService.refreshToken(request.refreshToken());
+    }
+
+}
