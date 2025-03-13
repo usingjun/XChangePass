cf0cfbc5d5ae0d4a4a7da37640f0a427229e583c
usingjun <usingjun99@gmail.com>
2025-03-13 11:03:10 +0900

    :sparkles: Feat: Jwt 토큰

---
+++ src/main/java/bumblebee/xchangepass/global/security/JwtAuthFilter.java
@@ -0,0 +1,63 @@
+package bumblebee.xchangepass.global.security;
+
+import bumblebee.xchangepass.domain.user.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String token = request.getHeader("Authorization");
+
+        String userId = null;
+
+        // Bearer token 검증 후 user name 조회
+        if(token != null && !token.isEmpty()) {
+            String jwtToken = token.substring(7);
+
+            userId = jwtProvider.getUserIdFromToken(jwtToken);
+        }
+
+        // token 검증 완료 후 SecurityContextHolder 내 인증 정보가 없는 경우 저장
+        if(userId != null && !userId.isEmpty() && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // Spring Security Context Holder 인증 정보 set
+            SecurityContextHolder.getContext().setAuthentication(getUserAuth(userId));
+        }
+
+        filterChain.doFilter(request,response);
+    }
+
+    /**
+     * token의 사용자 idx를 이용하여 사용자 정보 조회하고, UsernamePasswordAuthenticationToken 생성
+     *
+     * @param userEmail 사용자 email
+     * @return 사용자 UsernamePasswordAuthenticationToken
+     */
+    private UsernamePasswordAuthenticationToken getUserAuth(String userEmail) {
+        var userInfo = userService.readUserByUserId(userEmail);
+
+        return new UsernamePasswordAuthenticationToken(userInfo.userId(),
+                userInfo.password(),
+                Collections.singleton(new SimpleGrantedAuthority(userInfo.role().toString()))
+        );
+    }
+
+}
