550f7fee9e56256600ae1ec68b5cb9528538ea29
Si-rauis <kkyd7189@gmail.com>
2025-03-14 11:43:57 +0900

    ✨ Feat : 테스트 유저 초기화 로직 추가

---
+++ src/test/java/bumblebee/xchangepass/config/TestUserInitializer.java
@@ -0,0 +1,41 @@
+package bumblebee.xchangepass.config;
+
+import bumblebee.xchangepass.domain.user.dto.request.UserRegisterRequest;
+import bumblebee.xchangepass.domain.user.entity.Sex;
+import bumblebee.xchangepass.domain.user.repository.UserRepository;
+import bumblebee.xchangepass.domain.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.event.EventListener;
+
+import java.util.stream.IntStream;
+
+@TestConfiguration
+public class TestUserInitializer {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initTestUsers() {
+        userRepository.deleteAll();
+
+        IntStream.rangeClosed(1, 5).forEach(i -> {
+            UserRegisterRequest testUser = UserRegisterRequest.builder()
+                    .userEmail("Test" + i + "@gmail.com")
+                    .userPwd("Qwer1234!")
+                    .userName("테스터" + i)
+                    .userPhoneNumber("010-0000-000" + i)
+                    .userSex(i >= 4 ? Sex.FEMALE : Sex.MALE)
+                    .walletPassword("1234")
+                    .build();
+
+            userService.signupUser(testUser);
+        });
+    }
+}
+
