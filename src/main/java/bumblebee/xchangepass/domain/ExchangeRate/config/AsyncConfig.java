59f7ecb3fac9d484f23f0abe67dec3dd72792b82
CryingPerson <opnice12@naver.com>
2025-03-16 14:54:13 +0900

    ✨ Feat : config 파일 AsyncConfig 추가

---
+++ src/main/java/bumblebee/xchangepass/domain/ExchangeRate/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package bumblebee.xchangepass.domain.ExchangeRate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "asyncExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);  // 기본 스레드 수
+        executor.setMaxPoolSize(10);  // 최대 스레드 수
+        executor.setQueueCapacity(500);  // 작업 큐 크기
+        executor.setThreadNamePrefix("AsyncThread-");  // 스레드 이름 설정
+        executor.initialize();
+        return executor;
+    }
+}
