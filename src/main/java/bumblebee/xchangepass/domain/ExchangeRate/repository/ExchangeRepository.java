3859dddd385b46a3338040f341b5b2a2e147e32b
CryingPerson <opnice12@naver.com>
2025-03-16 16:07:02 +0900

    🔥 Remove: ExchangeRepository

--- src/main/java/bumblebee/xchangepass/domain/ExchangeRate/repository/ExchangeRepository.java
+++ src/main/java/bumblebee/xchangepass/domain/ExchangeRate/repository/ExchangeRepository.java
@@ -2,19 +2,11 @@ package bumblebee.xchangepass.domain.ExchangeRate.repository;
 
 
 import bumblebee.xchangepass.domain.ExchangeRate.entity.ExchangeRate;
+import bumblebee.xchangepass.domain.ExchangeRate.repository.exchangeTemp.ExchangeRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-public interface ExchangeRepository extends JpaRepository<ExchangeRate, Long> {
-
-
-    @Query("SELECT e FROM ExchangeRate e WHERE e.baseCurrency = :baseCurrency AND e.updatedAt >= :cacheThreshold")
-    List<ExchangeRate> findValidRates(@Param("baseCurrency") String baseCurrency, @Param("cacheThreshold") LocalDateTime cacheThreshold);
-
+public interface ExchangeRepository extends JpaRepository<ExchangeRate, Long> , ExchangeRepositoryCustom {
     List<ExchangeRate> findByBaseCurrency(String baseCurrency);
-
 }
