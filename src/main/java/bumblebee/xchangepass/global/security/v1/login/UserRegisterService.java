6d904e2452461c527d9fe3d265c197fe4b5a3b78
Si-rauis <kkyd7189@gmail.com>
2025-03-17 09:35:17 +0900

    ♻️ Refactor : 동기 처리 및 Transactional 추가

--- src/main/java/bumblebee/xchangepass/global/security/v1/login/UserRegisterService.java
+++ src/main/java/bumblebee/xchangepass/global/security/v1/login/UserRegisterService.java
@@ -1,8 +1,10 @@
 package bumblebee.xchangepass.global.security.v1.login;
 
 import bumblebee.xchangepass.domain.user.dto.request.UserRegisterRequest;
+import bumblebee.xchangepass.domain.user.entity.User;
 import bumblebee.xchangepass.domain.user.repository.UserRepository;
 import bumblebee.xchangepass.domain.user.service.NicknameGenerator;
+import bumblebee.xchangepass.domain.wallet.service.WalletService;
 import bumblebee.xchangepass.global.error.ErrorCode;
 import bumblebee.xchangepass.global.exception.CommonException;
 import bumblebee.xchangepass.global.util.DuplicateKeyExceptionHandler;
@@ -10,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class UserRegisterService {
 
     private final UserRepository userRepository;
     private final NicknameGenerator nicknameGenerator;
+    private final WalletService walletService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     /**
@@ -24,11 +28,16 @@ public class UserRegisterService {
      * 닉네임 Redis INCR을 활요한 자동 생성
      * 실명의 경우 추후 전화번호, 이메일에서 받아 오는 형식으로 변경 예정
      */
+    @Transactional
     public void signupUser(UserRegisterRequest request) {
         String uniqueNickname = null;
         try{
             uniqueNickname = nicknameGenerator.generateUniqueNickname();
-            userRepository.save(request.toEntity(bCryptPasswordEncoder, uniqueNickname));
+            User createUser = userRepository.save(request.toEntity(bCryptPasswordEncoder, uniqueNickname));
+            userRepository.flush();
+
+            // ✅ 지갑 생성 (동기 처리)
+            walletService.createWallet(createUser, bCryptPasswordEncoder.encode(request.walletPassword()));
         } catch (DataIntegrityViolationException e) {
             nicknameGenerator.rollbackNicknameId(uniqueNickname);
             DuplicateKeyExceptionHandler.handle(e);
