6da500ce81b46c558a0ce75e453656580f2dd0a6
usingjun <usingjun99@gmail.com>
2025-03-20 10:39:26 +0900

    ♻️ Refactor: wallet dto schema 달기 & fromEntity 추가

--- src/main/java/bumblebee/xchangepass/domain/wallet/wallet/dto/request/WalletTransferRequest.java
+++ src/main/java/bumblebee/xchangepass/domain/wallet/wallet/dto/request/WalletTransferRequest.java
@@ -1,6 +1,7 @@
 package bumblebee.xchangepass.domain.wallet.wallet.dto.request;
 
 import bumblebee.xchangepass.global.validation.ValidCurrency;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
@@ -9,16 +10,27 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Currency;
 
+@Schema(description = "송금 요청 객체")
 public record WalletTransferRequest(
+        //추후 수정 고려
+        @Schema(description = "받으시는 분 walletId", example = "1")
         @NotNull
         Long receiverWalletId,
+
+        @Schema(description = "송금할 금액", example = "10000.00")
         @DecimalMin(value = "1.00", message = "금액은 1.00 이상이어야 합니다.")
         @Digits(integer = 10, fraction = 2, message = "최대 10자리 정수와 소수점 2자리까지 가능합니다.")
         BigDecimal transferAmount,
+
+        @Schema(description = "가지고 계신 화폐 종류", example = "KRW, USD")
         @ValidCurrency
         Currency fromCurrency,
+
+        @Schema(description = "보내고자 하는 화폐 종류", example = "KRW, USD")
         @ValidCurrency
         Currency toCurrency,
+
+        @Schema(description = "송금 날짜", example = "2024-02-20T12:34:56")
         LocalDateTime transferDatetime
 ) {
 }
--- src/main/java/bumblebee/xchangepass/domain/wallet/wallet/dto/response/WalletBalanceResponse.java
+++ src/main/java/bumblebee/xchangepass/domain/wallet/wallet/dto/response/WalletBalanceResponse.java
@@ -1,9 +1,15 @@
 package bumblebee.xchangepass.domain.wallet.wallet.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.math.BigDecimal;
 
+@Schema(description = "화폐별 잔액 응답 객체")
 public record WalletBalanceResponse(
+        @Schema(description = "화폐 종류", example = "KRW, USD")
         String currency,
+
+        @Schema(description = "잔액", example = "10000.00")
         BigDecimal balance
 ) {
     public WalletBalanceResponse(String currency, BigDecimal balance) {
