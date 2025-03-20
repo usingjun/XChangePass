63613725e440a56a630a8e12c35b4bca401c2907
usingjun <usingjun99@gmail.com>
2025-03-20 09:26:59 +0900

    ♻️ Refactor: walletdto validation 추가

--- src/main/java/bumblebee/xchangepass/domain/wallet/dto/request/WalletInOutRequest.java
+++ src/main/java/bumblebee/xchangepass/domain/wallet/dto/request/WalletInOutRequest.java
@@ -1,18 +1,25 @@
 package bumblebee.xchangepass.domain.wallet.dto.request;
 
+import bumblebee.xchangepass.global.validation.ValidCurrency;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Currency;
 
 public record WalletInOutRequest(
-        Long userId,
+        @DecimalMin(value = "1.00", message = "금액은 1.00 이상이어야 합니다.")
+        @Digits(integer = 10, fraction = 2, message = "최대 10자리 정수와 소수점 2자리까지 가능합니다.")
         BigDecimal amount,
+        @ValidCurrency
         Currency fromCurrency,
+        @ValidCurrency
         Currency toCurrency,
         LocalDateTime chargeDatetime
 ) {
-    public WalletInOutRequest(Long userId, BigDecimal amount, Currency fromCurrency, Currency toCurrency, LocalDateTime chargeDatetime) {
-        this.userId = userId;
+    public WalletInOutRequest(BigDecimal amount, Currency fromCurrency, Currency toCurrency, LocalDateTime chargeDatetime) {
         this.amount = amount;
         this.fromCurrency = fromCurrency;
         this.toCurrency = toCurrency;
--- src/main/java/bumblebee/xchangepass/domain/wallet/dto/request/WalletTransferRequest.java
+++ src/main/java/bumblebee/xchangepass/domain/wallet/dto/request/WalletTransferRequest.java
@@ -1,15 +1,23 @@
 package bumblebee.xchangepass.domain.wallet.dto.request;
 
+import bumblebee.xchangepass.global.validation.ValidCurrency;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Currency;
 
 public record WalletTransferRequest(
-        Long senderWalletId,
+        @NotNull
         Long receiverWalletId,
+        @DecimalMin(value = "1.00", message = "금액은 1.00 이상이어야 합니다.")
+        @Digits(integer = 10, fraction = 2, message = "최대 10자리 정수와 소수점 2자리까지 가능합니다.")
         BigDecimal transferAmount,
+        @ValidCurrency
         Currency fromCurrency,
+        @ValidCurrency
         Currency toCurrency,
         LocalDateTime transferDatetime
 ) {
