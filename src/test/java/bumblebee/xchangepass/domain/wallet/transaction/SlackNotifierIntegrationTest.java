aef5da3afea25f9bfec27fd8daddef28f81f1a3c
usingjun <usingjun99@gmail.com>
2025-03-24 01:21:12 +0900

    :sparkles: Feature: 슬랙 테스트

---
+++ src/test/java/bumblebee/xchangepass/domain/wallet/transaction/SlackNotifierIntegrationTest.java
@@ -0,0 +1,27 @@
+package bumblebee.xchangepass.domain.wallet.transaction;
+
+import bumblebee.xchangepass.domain.wallet.wallet.consumer.SlackNotifier;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SlackNotifierIntegrationTest {
+
+    @Autowired
+    private SlackNotifier slackNotifier;
+
+    @Test
+    void sendSlackMessageTest() {
+        // given
+        String message = "✅ 테스트 메시지입니다 (통합 테스트)";
+
+        // when
+        slackNotifier.send(message);
+
+        // then
+        // 별도 assertion은 없고 Slack에서 수동으로 확인
+    }
+}
