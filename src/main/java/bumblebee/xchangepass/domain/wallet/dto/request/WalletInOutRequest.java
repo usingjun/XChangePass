19839fcfb6c75a4dd8d513869dc8491cd8b2fa01
CryingPerson <opnice12@naver.com>
2025-03-23 14:57:52 +0900

    ✨ Feat : WalletInOutRequest DTO Builder 패턴 추가

--- src/main/java/bumblebee/xchangepass/domain/wallet/dto/request/WalletInOutRequest.java
+++ src/main/java/bumblebee/xchangepass/domain/wallet/dto/request/WalletInOutRequest.java
@@ -1,21 +1,16 @@
 package bumblebee.xchangepass.domain.wallet.dto.request;
 
+import lombok.Builder;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Currency;
 
+@Builder
 public record WalletInOutRequest(
         Long userId,
         BigDecimal amount,
         Currency fromCurrency,
         Currency toCurrency,
         LocalDateTime chargeDatetime
-) {
-    public WalletInOutRequest(Long userId, BigDecimal amount, Currency fromCurrency, Currency toCurrency, LocalDateTime chargeDatetime) {
-        this.userId = userId;
-        this.amount = amount;
-        this.fromCurrency = fromCurrency;
-        this.toCurrency = toCurrency;
-        this.chargeDatetime = chargeDatetime;
-    }
-}
+){}
