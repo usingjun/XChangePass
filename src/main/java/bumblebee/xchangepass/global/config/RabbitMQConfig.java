7ae808e7c48000aeabf7eb01308465a38cb99e59
usingjun <usingjun99@gmail.com>
2025-03-24 01:21:01 +0900

    :sparkles: Feature: RabbitMQConfig retry 설정 추가

--- src/main/java/bumblebee/xchangepass/global/config/RabbitMQConfig.java
+++ src/main/java/bumblebee/xchangepass/global/config/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package bumblebee.xchangepass.global.config;
 
-import org.springframework.amqp.core.Queue;
+import org.aspectj.apache.bcel.generic.RET;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -9,6 +10,11 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static bumblebee.xchangepass.global.common.Constants.*;
+
 
 @EnableRabbit
 @Configuration
@@ -16,7 +22,37 @@ public class RabbitMQConfig {
 
     @Bean
     public Queue transactionQueue() {
-        return new Queue("transaction-status-queue", true);
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-dead-letter-exchange", "");
+        args.put("x-dead-letter-routing-key", RETRY_QUEUE);
+        return new Queue(WALLET_TRANSACTION, true, false, false, args);
+    }
+
+    @Bean
+    public Queue retryQueue() {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-dead-letter-exchange", "");
+        args.put("x-dead-letter-routing-key", WALLET_TRANSACTION);
+        args.put("x-message-ttl", 5000);
+
+        return new Queue(RETRY_QUEUE, true, false, false, args);
+    }
+
+    @Bean
+    public Queue deadLetterQueue() {
+        return QueueBuilder.durable(DLQ_NAME).build();
+    }
+
+    @Bean
+    public DirectExchange deadLetterExchange() {
+        return new DirectExchange(DLX_NAME);
+    }
+
+    @Bean
+    public Binding deadLetterBinding() {
+        return BindingBuilder.bind(deadLetterQueue())
+                .to(deadLetterExchange())
+                .with(DLQ_ROUTING_KEY);
     }
 
     @Bean
@@ -24,7 +60,6 @@ public class RabbitMQConfig {
         return new Jackson2JsonMessageConverter();
     }
 
-    // ✅ RabbitTemplate에 메시지 컨버터 지정
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
                                          MessageConverter jsonMessageConverter) {
