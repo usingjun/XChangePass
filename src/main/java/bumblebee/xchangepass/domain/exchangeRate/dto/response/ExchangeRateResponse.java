de5d79aa70ed29b2d51d236ea8eabeec98b9de83
CryingPerson <opnice12@naver.com>
2025-03-23 17:16:17 +0900

    🔥 Remove : 불필요한 주석 삭제

--- src/main/java/bumblebee/xchangepass/domain/exchangeRate/dto/response/ExchangeRateResponse.java
+++ src/main/java/bumblebee/xchangepass/domain/exchangeRate/dto/response/ExchangeRateResponse.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
-import java.io.Serializable;
 import java.util.Map;
 
 @Builder
