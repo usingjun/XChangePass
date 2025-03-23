eabbd08af85bbfec23bcb17235940247a8317278
usingjun <usingjun99@gmail.com>
2025-03-23 14:24:02 +0900

    :sparkles: Feature: RabbitMQConfig json Converter 설정

--- src/main/java/bumblebee/xchangepass/global/config/RabbitMQConfig.java
+++ src/main/java/bumblebee/xchangepass/global/config/RabbitMQConfig.java
@@ -2,6 +2,10 @@ package bumblebee.xchangepass.global.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +17,19 @@ public class RabbitMQConfig {
     @Bean
     public Queue transactionQueue() {
         return new Queue("transaction-status-queue", true);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    // ✅ RabbitTemplate에 메시지 컨버터 지정
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         MessageConverter jsonMessageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter);
+        return rabbitTemplate;
     }
 }
