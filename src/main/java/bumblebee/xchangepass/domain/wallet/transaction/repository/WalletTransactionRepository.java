1b6b9410f2a694b6fc5684fb54d99bffe1f6b836
usingjun <usingjun99@gmail.com>
2025-03-26 09:44:46 +0900

    ✨ Feat: transaction page repository

--- src/main/java/bumblebee/xchangepass/domain/wallet/transaction/repository/WalletTransactionRepository.java
+++ src/main/java/bumblebee/xchangepass/domain/wallet/transaction/repository/WalletTransactionRepository.java
@@ -1,6 +1,7 @@
 package bumblebee.xchangepass.domain.wallet.transaction.repository;
 
 import bumblebee.xchangepass.domain.wallet.transaction.entity.WalletTransaction;
+import bumblebee.xchangepass.domain.wallet.transaction.repository.search.WalletTransactionRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, Long> {
+public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, Long>, WalletTransactionRepositoryCustom {
     @Query("""
                 select t from WalletTransaction t
                 where t.myWallet.walletId=:walletId or t.counterWallet.walletId=:walletId
