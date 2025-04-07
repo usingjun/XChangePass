409ad86db757ea85a9a6a7e5e70f41c54ab4dbe7
usingjun <usingjun99@gmail.com>
2025-04-07 22:47:23 +0900

    :sparkles: Feat: 이상감지 dto 생성

---
+++ src/main/java/bumblebee/xchangepass/domain/wallet/fraud/service/FraudDetectEvent.java
@@ -0,0 +1,11 @@
+package bumblebee.xchangepass.domain.wallet.fraud.service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record FraudDetectEvent(
+        Long userId,
+        BigDecimal amount,
+        LocalDateTime timestamp,
+        String detail
+) {}
