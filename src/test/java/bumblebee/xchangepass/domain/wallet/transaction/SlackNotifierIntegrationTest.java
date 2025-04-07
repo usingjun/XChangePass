a36e277f7e99520ae2f5d4098b59e16d60243ab4
usingjun <usingjun99@gmail.com>
2025-04-07 22:45:54 +0900

    ♻️ Refactor: SlackNotifier 수정 및 이상감지 알림

--- src/test/java/bumblebee/xchangepass/domain/wallet/transaction/SlackNotifierIntegrationTest.java
+++ src/test/java/bumblebee/xchangepass/domain/wallet/transaction/SlackNotifierIntegrationTest.java
@@ -14,12 +14,12 @@ class SlackNotifierIntegrationTest {
     private SlackNotifier slackNotifier;
 
     @Test
-    void sendSlackMessageTest() {
+    void failToSaveTransactionSlackMessageTest() {
         // given
         String message = "✅ 테스트 메시지입니다 (통합 테스트)";
 
         // when
-        slackNotifier.send(message);
+        slackNotifier.failToSaveTransaction(message);
 
         // then
         // 별도 assertion은 없고 Slack에서 수동으로 확인
