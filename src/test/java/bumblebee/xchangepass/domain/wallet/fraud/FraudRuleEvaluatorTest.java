fdbaf5710033b307917772060575974bb7aaecb1
usingjun <usingjun99@gmail.com>
2025-04-08 12:24:05 +0900

    ✅ Test: 테스트 코드 수정

--- src/test/java/bumblebee/xchangepass/domain/wallet/fraud/FraudRuleEvaluatorTest.java
+++ src/test/java/bumblebee/xchangepass/domain/wallet/fraud/FraudRuleEvaluatorTest.java
@@ -1,19 +1,50 @@
 package bumblebee.xchangepass.domain.wallet.fraud;
 
+import bumblebee.xchangepass.config.TestUserInitializer;
 import bumblebee.xchangepass.domain.wallet.fraud.service.FraudRuleEvaluator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Testcontainers
+@Import(TestUserInitializer.class)
 @SpringBootTest
 class FraudRuleEvaluatorTest {
 
     @Autowired
     private FraudRuleEvaluator fraudRuleEvaluator;
+
+    @Container
+    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:16")
+//            .withInitScript("init.sql")
+            .withDatabaseName("xcp_test")
+            .withUsername("postgres")
+            .withPassword("postgres");
+
+    @Container
+    static GenericContainer<?> redisContainer = new GenericContainer<>(DockerImageName.parse("redis:7"))
+            .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void overrideRedisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.host", redisContainer::getHost);
+        registry.add("spring.redis.port", () -> redisContainer.getMappedPort(6379));
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
 
     @Test
     void test_Lua_script_detects_no_fraud_on_single_transaction() {
@@ -31,12 +62,13 @@ class FraudRuleEvaluatorTest {
         Long userId = 1002L;
         BigDecimal amount = new BigDecimal("20000");
 
-        // 5건 연속 트랜잭션으로 빈도 룰 유도
-        for (int i = 0; i < 5; i++) {
-            fraudRuleEvaluator.isSuspicious(userId, amount);
-        }
+        Boolean suspicious = false;
 
-        boolean suspicious = fraudRuleEvaluator.isSuspicious(userId, amount);
+        // 10건 연속 트랜잭션으로 빈도 룰 유도
+        for (int i = 0; i < 10; i++) {
+            if(suspicious) break;
+            suspicious = fraudRuleEvaluator.isSuspicious(userId, amount);
+        }
 
         assertThat(suspicious).isTrue();
     }
